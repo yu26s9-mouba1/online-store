3d7d84f25e796b974825fde96094583e41ea1870
Moubarak Congacou <cmouba1999@gmail.com>
2026-04-24 11:22:08 -0400

    Loaded products to the store

--- src/main/java/com/pluralsight/Products.java
+++ src/main/java/com/pluralsight/Products.java
@@ -1,4 +1,49 @@
 package com.pluralsight;
 
 public class Products {
+
+    private String productNane;
+    private int productSKU;
+    private double productPrice;
+    private String productDepartment;
+
+    public Products(String productNane, int productSKU, double productPrice, String productDepartment) {
+        this.productNane = productNane;
+        this.productSKU = productSKU;
+        this.productPrice = productPrice;
+        this.productDepartment = productDepartment;
+    }
+
+    public String getProductNane() {
+        return productNane;
+    }
+
+    public void setProductNane(String productNane) {
+        this.productNane = productNane;
+    }
+
+    public int getProductSKU() {
+        return productSKU;
+    }
+
+    public void setProductSKU(int productSKU) {
+        this.productSKU = productSKU;
+    }
+
+    public double getProductPrice() {
+        return productPrice;
+    }
+
+    public void setProductPrice(double productPrice) {
+        this.productPrice = productPrice;
+    }
+
+    public String getProductDepartment() {
+        return productDepartment;
+    }
+
+    public void setProductDepartment(String productDepartment) {
+        this.productDepartment = productDepartment;
+    }
 }
+
--- src/main/java/com/pluralsight/StoreApp.java
+++ src/main/java/com/pluralsight/StoreApp.java
@@ -1,7 +1,56 @@
 package com.pluralsight;
+import java.io.IOException;
+import java.util.HashMap;
+import java.util.Scanner;
+import java.io.BufferedReader;
+import java.io.FileReader;
 
 public class StoreApp {
     public static void main(String[] args) {
 
+        Scanner scanner = new Scanner(System.in);
+
+        
+
+
+
     }
 }
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+//old
+//String filenameOld = "products.csv";
+//new
+//String filename = "data/products.csv";
